f407abc8e929ce4fdff1557aef0ee2ebe26f2201
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-07-25 22:32:37 -0400

    Remove store

--- frontend/src/app/shared/root-store/root-store.state.ts
+++
@@ -1,8 +0,0 @@
-// import {
-// 	State as CurrentUserState,
-// 	InitialState as CurrentUserInitialState
-// } from '../shared/store/current-user-store/current-user-store.state';
-
-export interface State {
-	// currentUser: CurrentUserState
-}
