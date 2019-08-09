594e3d50a0b5b19cf80812d0d83d6d4ffa924dd7
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-08-09 01:05:55 -0400

    Implement cors

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from referral_links.routes import referral_links
 
 app = Flask(__name__)
+CORS(app, origins="*")
 app.register_blueprint(referral_links)
 
 if __name__ == '__main__':
--- api/referral_links/routes.py
+++ api/referral_links/routes.py
@@ -8,5 +8,7 @@ referral_links = Blueprint("referral_links", __name__)
 @referral_links.route("/referral_links", methods=["GET"])
 def get_referral_links():
     dbc = db.cursor()
-    dbc.execute("SELECT `key`, `title`, `link`, `order` FROM referralLinks ORDER BY `order`")
-    return jsonify(dbc.fetchall())
+    dbc.execute(
+        "SELECT `key`, `title`, `link`, `order` FROM referralLinks ORDER BY `order`"
+    )
+    return jsonify({"data": {"referralLinks": dbc.fetchall()}})
