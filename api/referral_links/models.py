ffbe213e68fbfb82086733dc392b5fc6a650a1e2
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-08-19 09:13:09 -0400

    Try djmodels

--- api/referral_links/models.py
+++ api/referral_links/models.py
@@ -1,4 +1,18 @@
-class ReferralLinks:
+# class ReferralLink:
 
-    def __init__(self, **props):
-        id = props.get('id', None)
+#     def __init__(self, **props):
+#         fields = ['key', 'title', 'link', 'order']
+#         for field in fields:
+#             setattr(self, field, props.get(field, None))
+
+from djmodels.db import models
+
+
+class ReferralLink(models.Model):
+    key = models.CharField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255, default=None)
+    link = models.CharField(max_length=255, default=None)
+    order = models.IntegerField()
+
+    class Meta:
+        db_table = "referral_links"
