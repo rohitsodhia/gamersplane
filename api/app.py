79d21bda4f47ad4c054ce6a9da047a710756d541
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-08-24 21:27:26 -0400

    Setup systems

--- api/app.py
+++ api/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+
 from referral_links.routes import referral_links
+from systems.routes import systems
 
 app = Flask(__name__)
 CORS(app, origins="*")
+
 app.register_blueprint(referral_links)
+app.register_blueprint(systems)
 
 if __name__ == '__main__':
     app.run()
--- api/systems/models.py
+++ api/systems/models.py
@@ -1,16 +1,14 @@
 class System:
-    def __init__(self, **props):
-        fields = [
-            "id",
-            "name",
-            "sortName",
-            "publisher",
-            "genres",
-            "basics",
-            "hasCharSheet",
-            "enabled",
-            "createdOn",
-            "updatedOn",
-        ]
-        for field in fields:
-            self[field] = props.get(field, None)
+
+    _fields = [
+        "id",
+        "name",
+        "sortName",
+        "publisher",
+        "genres",
+        "basics",
+        "hasCharSheet",
+        "enabled",
+        "createdOn",
+        "updatedOn",
+    ]
