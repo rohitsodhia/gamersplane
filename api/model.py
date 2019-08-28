9ea4bb25117e78b364d2fb4f216e8526009dac91
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-08-28 00:02:59 -0400

    Create model base

---
+++ api/model.py
@@ -0,0 +1,21 @@
+class Model:
+
+    _fields = []
+    _values = {}
+
+    def __init__(self, **props):
+        for key, value in props.items():
+            setattr(self, key, value)
+
+    def __setattr__(self, name, value):
+        if name not in self._fields:
+            return
+        self._values[name] = value
+
+    def __getattr__(self, name):
+        if name in ["_fields", "_values"]:
+            return getattr(self, name)
+        try:
+            return self._values[name]
+        except AttributeError:
+            raise
