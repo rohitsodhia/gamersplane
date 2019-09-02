101f2a611386fa68e8921d2b2116b1339ee9f89c
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-09-02 18:04:48 -0400

    Catch errors

--- api/auth/routes.py
+++ api/auth/routes.py
@@ -10,13 +10,18 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/auth/login", methods=["POST"])
 def login():
+    try:
+        email = request.json["email"]
+    except KeyError:
+        return jsonify({"errors": [{"id": "no_email"}]})
     dbc = db.cursor()
-    dbc.execute(
-        "SELECT * FROM users WHERE email = %(email)s", {"email": "rohit@rhovisions.com"}
-    )
+    dbc.execute("SELECT * FROM users WHERE email = %(email)s", {"email": email})
     if dbc.rowcount:
         user = User(**dbc.fetchone())
-        password = request.json["password"]
+        try:
+            password = request.json["password"]
+        except KeyError:
+            return jsonify({"errors": [{"id": "no_password"}]})
         if user.validate_pass(password):
             return jsonify({"data": {"logged_in": True, "jwt": user.generate_jwt()}})
     return jsonify({"errors": [{"id": "invalid_user"}]})
