e3c3df0e7a8975f2c2ef8b558c3a4b69f1d2c3e4
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-09-15 23:22:02 -0400

    Use a comprehension to simplify code

--- api/systems/routes.py
+++ api/systems/routes.py
@@ -3,6 +3,8 @@ from flask import Blueprint, jsonify, request
 
 from database import db
 
+from systems.models import System
+
 systems = Blueprint("systems", __name__)
 
 
@@ -12,7 +14,7 @@ def get_systems():
     if request.values.get("basic"):
         fields = "`id`, `name`, `sortName`"
     else:
-        fields = "`id`, `name`, `sortName`, `publisher`, `genres`, `basics`, `hasCharSheet`, `enabled`, `createdOn`, `updatedOn`"
+        fields = ", ".join(f"`{}`" for field in System.get_fields())
     dbc.execute(f"SELECT {fields} FROM systems WHERE enabled = 1 ORDER BY `sortName`")
     systems = dbc.fetchall()
     for system in systems:
