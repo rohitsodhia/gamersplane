dd58f675a3774270a69bca284fc7e0dc27c541e9
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-11-03 00:42:08 -0400

    Swap to django cursor

--- api/src/referral_links/routes.py
+++ api/src/referral_links/routes.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, jsonify
-
-from database import db
+from django.db import connection
 
 referral_links = Blueprint("referral_links", __name__)
 
 
 @referral_links.route("/referral_links", methods=["GET"])
 def get_referral_links():
-    dbc = db.cursor()
-    dbc.execute(
-        "SELECT `key`, `title`, `link`, `order` FROM referralLinks ORDER BY `order`"
-    )
-    return jsonify({"data": {"referralLinks": dbc.fetchall()}})
+    with connection.cursor() as dbc:
+        dbc.execute(
+            "SELECT `key`, `title`, `link`, `order` FROM referralLinks ORDER BY `order`"
+        )
+        return jsonify({"data": {"referralLinks": dbc.fetchall()}})
--- api/src/systems/routes.py
+++ api/src/systems/routes.py
@@ -1,7 +1,6 @@
 import json
 from flask import Blueprint, jsonify, request
-
-from database import db
+from django.db import connection
 
 from systems.models import System
 from systems.utils import generate_systems_sql
@@ -11,12 +10,12 @@ systems = Blueprint("systems", __name__)
 
 @systems.route("/systems", methods=["GET"])
 def get_systems():
-    dbc = db.cursor()
-    sql = generate_systems_sql(request.values.get("basic"))
-    dbc.execute(sql)
-    systems = dbc.fetchall()
-    for system in systems:
-        for field in ["publisher", "genres", "basics"]:
-            if field in system and system[field]:
-                system[field] = json.loads(system[field])
-    return jsonify({"data": {"systems": systems}})
+    with connection.cursor() as dbc:
+        sql = generate_systems_sql(request.values.get("basic"))
+        dbc.execute(sql)
+        systems = dbc.fetchall()
+        for system in systems:
+            for field in ["publisher", "genres", "basics"]:
+                if field in system and system[field]:
+                    system[field] = json.loads(system[field])
+        return jsonify({"data": {"systems": systems}})
