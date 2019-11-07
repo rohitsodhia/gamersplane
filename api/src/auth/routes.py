151a7545266ce29ae38a9171d7b42727b4021aad
Rohit Sodhia <sodhia.rohit@gmail.com>
2019-11-06 20:59:55 -0500

    Reorganize register for better errors

--- api/src/auth/routes.py
+++ api/src/auth/routes.py
@@ -30,14 +30,29 @@ def login():
 
 @auth.route("/auth/register", methods=["POST"])
 def register():
+    errors = []
     try:
         email = request.json["email"]
     except KeyError:
-        return jsonify({"errors": [{"id": "no_email"}]})
+        errors.push({"id": "no_email"})
+
     try:
         username = request.json["username"]
     except KeyError:
-        return jsonify({"errors": [{"id": "no_username"}]})
+        errors.push({"id": "no_username"})
+
+    password = request.json["password"]
+    rep_password = request.json["rep_password"]
+    if password != rep_password:
+        errors.push({"id": "pass_mismatch"})
+
+    pass_valid = User.validate_pass(password)
+    if pass_valid is not True:
+        errors.push({"id": f"pass_{pass_valid}"})
+
+    if len(errors):
+        return jsonify({"errors": errors})
+
     with connection.cursor() as dbc:
         dbc.execute(
             "SELECT email, username FROM users WHERE email = %(email)s OR username = %(username)s",
@@ -51,11 +66,3 @@ def register():
             if reg_username == username:
                 errors.push("username_taken")
             return jsonify({"errors": [[{"id": error}] for error in errors]})
-
-        password = request.json["password"]
-        rep_password = request.json["rep_password"]
-        if password != rep_password:
-            return jsonify({"errors": [{"id": "pass_mismatch"}]})
-        pass_valid = User.validate_pass(password)
-        if pass_valid is not True:
-            return jsonify({"errors": [{"id": pass_valid}]})
