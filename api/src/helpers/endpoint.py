76a1750031aa229eca81defba7c8419db747aa7c
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-02-29 01:03:37 -0500

    Required fields should not be blank

--- api/src/helpers/endpoint.py
+++ api/src/helpers/endpoint.py
@@ -1,6 +1,6 @@
 def require_values(data_obj, fields=[]):
     missing_fields = []
     for key in fields:
-        if key not in data_obj:
+        if key not in data_obj or not data_obj[key]:
             missing_fields.append(key)
     return missing_fields
