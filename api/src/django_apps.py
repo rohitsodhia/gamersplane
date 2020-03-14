a32b162496d0f383528654ff754fef084aff0e23
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-03-13 22:34:49 -0400

    Automated app detection isn't working

--- api/src/django_apps.py
+++ api/src/django_apps.py
@@ -1,7 +1,1 @@
-import os
-
-exclude = ["helpers"]
-apps = []
-for ele in os.listdir("."):
-    if os.path.isdir(ele) and ele not in exclude:
-        apps.append(ele)
+apps = ["auth", "referral_links", "systems"]
