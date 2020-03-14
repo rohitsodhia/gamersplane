3b4bf805da405eee733bbd2289a951ed52fbbff9
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-03-13 22:30:08 -0400

    Get tests finally working

--- api/tests/django_test_conf.py
+++ api/tests/django_test_conf.py
@@ -1,13 +1,8 @@
-import django
-from django.conf import settings
-
 from django_apps import apps
 
 
-settings.configure(
-    DATABASES={
-        "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "test_gamersplane"}
-    },
-    INSTALLED_APPS=apps,
-)
-django.setup()
+DATABASES = {
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "test_gamersplane"}
+}
+INSTALLED_APPS = apps
+SECRET_KEY = "asdf"
