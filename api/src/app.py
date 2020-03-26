e53ae66622081c1a80255af3ad0c3e0ce286f201
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-03-25 21:46:28 -0400

    Create middleware to authenticate user

--- api/src/app.py
+++ api/src/app.py
@@ -3,12 +3,15 @@ import django_conf
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+import middleware
 from referral_links.routes import referral_links
 from systems.routes import systems
 from auth.routes import auth
 
 app = Flask(__name__)
 CORS(app, origins="*")
+
+app.before_request(middleware.validate_jwt)
 
 app.register_blueprint(referral_links)
 app.register_blueprint(systems)
---
+++ api/src/middleware.py
@@ -0,0 +1,25 @@
+import os
+import jwt
+
+from flask import g, request
+
+from auth.models import User
+
+
+def validate_jwt():
+    g.user = None
+
+    jwt_token = request.headers.get("Authorization")
+    if jwt_token:
+        jwt_token = jwt_token[7:]
+        try:
+            jwt_body = jwt.decode(
+                jwt_token,
+                os.getenv("JWT_SECRET_KEY"),
+                algorithm=os.getenv("JWT_ALGORITHM"),
+            )
+        except jwt.ExpiredSignatureError:
+            return
+
+        user = User.objects.get(id=jwt_body["user_id"])
+        g.user = user
