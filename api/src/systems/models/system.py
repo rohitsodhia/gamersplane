01a3f0e00a41e3b0b5454a798dc043a29b948ab8
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-03-26 00:56:18 -0400

    Standardize how models are setup

--- api/src/systems/models/system.py
+++ api/src/systems/models/system.py
@@ -1,8 +1,8 @@
-from django.db.models import Model, CharField, BooleanField, DateTimeField, Manager
+from django.db import models
 from django_mysql.models import JSONField
 
 
-class EnabledSystemsManager(Manager):
+class EnabledSystemsManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(enabled=True)
 
@@ -10,20 +10,20 @@ class EnabledSystemsManager(Manager):
         return self.get_queryset().only("id", "name", "sortName")
 
 
-class System(Model):
+class System(models.Model):
     class Meta:
         db_table = "systems"
 
-    id = CharField(max_length=20, primary_key=True)
-    name = CharField(max_length=40)
-    sortName = CharField(max_length=40)
+    id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=40)
+    sortName = models.CharField(max_length=40)
     publisher = JSONField(null=True)
     generes = JSONField(null=True)
     basics = JSONField(null=True)
-    hasCharSheet = BooleanField(default=True)
-    enabled = BooleanField(default=True)
-    createdOn = DateTimeField(auto_now_add=True)
-    updatedOn = DateTimeField(auto_now=True)
+    hasCharSheet = models.BooleanField(default=True)
+    enabled = models.BooleanField(default=True)
+    createdOn = models.DateTimeField(auto_now_add=True)
+    updatedOn = models.DateTimeField(auto_now=True)
 
     objects = EnabledSystemsManager()
-    all_objects = Manager()
+    all_objects = models.Manager()
