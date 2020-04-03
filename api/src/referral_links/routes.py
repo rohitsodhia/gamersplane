ca0da588aad8519ad9e6bca515613a5d301bde9a
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-04-02 23:56:40 -0400

    Swap response object in for jsonifys

--- api/src/referral_links/routes.py
+++ api/src/referral_links/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
+
+from helpers.response import response
 
 from referral_links.models import ReferralLink
 
@@ -8,4 +10,4 @@ referral_links = Blueprint("referral_links", __name__)
 @referral_links.route("/referral_links", methods=["GET"])
 def get_referral_links():
     links = ReferralLink.objects.order_by("order").values()
-    return jsonify({"data": {"referralLinks": [link for link in links]}})
+    return response.success({"referralLinks": [link for link in links]})
