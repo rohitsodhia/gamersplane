dd39a7efbffffabc7712b67ff1f920311c1f8b34
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-04-08 22:01:49 -0400

    Create models for soft delete and timestamps

---
+++ api/src/helpers/base_models.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from django.db import models
+
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        return super().update(deleted_at=datetime.utcnow())
+
+    def hard_delete(self):
+        return super().delete()
+
+    def alive(self):
+        return self.filter(deleted_at=None)
+
+    def dead(self):
+        return self.exclude(deleted_at=None)
+
+
+class SoftDeleteManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop("alive_only", True)
+        super().__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.alive_only:
+            return SoftDeleteQuerySet(self.model).alive()
+        return SoftDeleteQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+
+class SoftDeleteModel(models.Model):
+    class Meta:
+        abstract = True
+
+    deletedAt = models.DateTimeField(null=True)
+
+    objects = SoftDeleteManager()
+    all_objects = SoftDeleteManager(alive_only=False)
+
+    def delete(self):
+        self.deletedAt = datetime.utcnow()
+        self.save()
+
+    def hard_delete(self):
+        super().delete()
+
+
+class TimestampedModel(models.Model):
+    class Meta:
+        abstract = True
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
