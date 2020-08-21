9ab5d37133ea7e75c2c60edbaf9a464f8aad3b25
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-08-21 00:49:57 -0400

    Cleanup imports

--- api/src/auth/roles_routes.py
+++ api/src/auth/roles_routes.py
@@ -1,13 +1,12 @@
 import json
 
 from flask import Blueprint, request
-from django.db import connection
 
 from helpers.decorators import logged_in
 from helpers.response import response
 from helpers.endpoint import require_values
 
-from auth.models import User, PasswordReset
+from auth.models import User
 
 roles = Blueprint("roles", __name__, url_prefix="/roles")
 
