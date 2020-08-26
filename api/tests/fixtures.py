bae4f688eefdd8686e8e9f7caaf0b4a7c405bd42
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-08-26 00:32:25 -0400

    Create an app fixture for tests

---
+++ api/tests/fixtures.py
@@ -0,0 +1,13 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config["TESTING"] = True
+
+    with app.app_context():
+        with app.test_client() as client, app.test_request_context():
+            yield client
