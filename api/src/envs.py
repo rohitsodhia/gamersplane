8aa48c254a23f91b173f7a5748c7acfa0f2faa98
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-09-11 22:39:37 -0400

    Updated envs

--- api/src/envs.py
+++ api/src/envs.py
@@ -4,6 +4,9 @@ ENVIRONMENT = os.getenv("FLASK_ENV", "dev")
 
 SERVER_NAME = os.getenv("SERVER_NAME")
 
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
