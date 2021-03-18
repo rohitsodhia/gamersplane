b3e1467b3996067de7b901e0cbffc9310ba6a07c
Rohit Sodhia <sodhia.rohit@gmail.com>
2021-03-18 10:52:48 -0400

    Create individual user GET endpoint

--- api/src/users/routes.py
+++ api/src/users/routes.py
@@ -3,8 +3,15 @@ from flask import Blueprint, request
 from helpers.response import response
 from helpers.endpoint import require_values
 
+from users.models import User
+
 users = Blueprint("users", __name__, url_prefix="/users")
 
 
-# @users.route("/login", methods=["POST"])
-# def login():
+@users.route("/<id>", methods=["GET"])
+def get_user(id):
+    try:
+        user = User.objects.get(id=id)
+    except User.DoesNotExist:
+        return response.errors({"noUser": True})
+    return response.success({"user": user.to_dict()})
