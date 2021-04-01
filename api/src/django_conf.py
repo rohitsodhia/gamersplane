8b350deb795a8335e10fd83b0b6dcdeeaefa5bf5
Rohit Sodhia <sodhia.rohit@gmail.com>
2021-04-01 09:17:04 -0400

    Add redis to the stack

--- api/src/django_conf.py
+++ api/src/django_conf.py
@@ -3,6 +3,10 @@ import os
 from django_apps import apps
 
 
+def make_key(key, key_prefix, version):
+    return key
+
+
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DATABASES = {
     "default": {
@@ -12,6 +16,18 @@ DATABASES = {
         "PASSWORD": os.getenv("MYSQL_PASSWORD"),
         "HOST": os.getenv("MYSQL_HOST"),
         "PORT": "3306",
+    }
+}
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS_HOST"),
+        "TIMEOUT": os.getenv("REDIS_TTL"),
+        "OPTIONS": {
+            "MAX_ENTRIES": 1000000,
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_FUNCTION": make_key,
     }
 }
 INSTALLED_APPS = apps
