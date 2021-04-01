7dce3eea2be4c7f3adb8199fabc0fb8e97f8359c
Rohit Sodhia <sodhia.rohit@gmail.com>
2021-04-01 09:38:20 -0400

    Swap to using envs file instead of os.getenv

--- api/src/envs.py
+++ api/src/envs.py
@@ -12,4 +12,14 @@ MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_TTL = os.getenv("REDIS_TTL")
+
+DJANGO_SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+EMAIL_URI = os.getenv("EMAIL_URI")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_LOGIN = os.getenv("EMAIL_LOGIN")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
