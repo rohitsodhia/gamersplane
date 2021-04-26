0505056ee7a9c99bc38f1c7486745ac9731b557b
Rohit Sodhia <sodhia.rohit@gmail.com>
2021-04-25 22:46:57 -0400

    Serialize forum for return

--- api/src/forums/forums_routes.py
+++ api/src/forums/forums_routes.py
@@ -5,6 +5,7 @@ from helpers.response import response
 from helpers.cache import CACHE_KEYS
 
 from forums.models import Forum
+from forums.serializers import ForumSerializer
 
 forums = Blueprint("forums", __name__, url_prefix="/forums")
 
@@ -17,3 +18,7 @@ def get_forums(forum_id: int = 0):
     else:
         forum = Forum.objects.get(id=forum_id)
     cache.set(CACHE_KEYS["forum_details"].format(forum_id=forum_id), forum)
+
+    serialized_forum = ForumSerializer(forum)
+
+    return response.success(data={"forum": serialized_forum.data})
